0f22764d9ba27027b2cb20f6c2d8f7669389a758
evo <evo.annus@gmail.com>
2025-10-30 21:55:38 +0200

    added calibration

--- src/main.cpp
+++ src/main.cpp
@@ -7,63 +7,104 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SD
 #define RXD2 5
 #define TXD2 6
 
-byte data[] = { 0xFD, 0x00, 0x00, 0x00, 0x00, 0x00 };
+byte get_data[] = { 0xFD, 0x00, 0x00, 0x00, 0x00, 0x00 };
 byte calib[] = { 0xFE, 0x7A, 0x47, 0x46, 0x00, 0x00 }; 
+
+unsigned long startMillis; 
+unsigned long currentMillis;
 
 HardwareSerial senSerial(0);
 
 void readHrSensor(int *hrData);
+bool calibrateSensor();
 
 void setup() {
-  // put your setup code here, to run once:
-  //int result = myFunction(2, 3);
   u8g2.begin();
+  u8g2.setFont(u8g2_font_6x13_tf);
+  u8g2.clearBuffer();
+  
   Serial.begin(115200);
   senSerial.begin(115200, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("Serial 2 started at 115200 baud rate");
-  senSerial.write(calib, sizeof(calib));
-  delay(10000);
+
+
+  if(calibrateSensor()) {
+    u8g2.drawStr(0, 40, "Calibrate Success");
+  } else {
+    u8g2.drawStr(0, 40, "Calibrate Failed");
+  }
+  u8g2.sendBuffer();
+  delay(3000);
+  u8g2.setPowerSave(1);
+
+  startMillis = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  u8g2.clearBuffer();					// clear the internal memory
-  u8g2.setFont(u8g2_font_6x13_tf);	// choose a suitable font
-
-
+  u8g2.clearBuffer(); // clear the internal memory
 
   int hrData[] = {255, 255, 255};
-  readHrSensor(hrData);
 
-  String mes1 = "Heart rate: " + (String)hrData[2];
-  String mes2 = "Blood p H/L: " + (String)hrData[0] + "/" + (String)hrData[1];
-  Serial.println(mes1);
-  Serial.println(mes2);
+  currentMillis = millis();
+  if (currentMillis - startMillis >= 1000) {
+    readHrSensor(hrData);
+    String mes1 = "Heart rate: " + (String)hrData[2];
+    String mes2 = "Blood p H/L: " + (String)hrData[0] + "/" + (String)hrData[1];
+    Serial.println(mes1);
+    Serial.println(mes2);
+    Serial.println("-------------------------------");
+    startMillis = currentMillis;  
+  }
  
 
-  u8g2.drawStr(0, 20, mes1.c_str());
-  u8g2.drawStr(0, 40, mes2.c_str());	// write something to the internal memory
-  u8g2.sendBuffer();
+  //u8g2.drawStr(0, 20, mes1.c_str());
+  //u8g2.drawStr(0, 40, mes2.c_str());	// write something to the internal memory
+  //u8g2.sendBuffer();
 
 
-  delay(1000);
-  Serial.println("-------------------------------");
+  // delay(1000);
+  // u8g2.setPowerSave(1);
+  // delay(2000);
+  // u8g2.setPowerSave(0);
 }
 
 
 // Read sensor data
 void readHrSensor(int *hrData) {
-  senSerial.write(data, sizeof(data));
+  senSerial.write(get_data, sizeof(get_data));
   while (!senSerial.available());
 
   while (senSerial.available()) {
     byte a = senSerial.read();
-    delay(10);
     if (int(a) == 253) {
       for (size_t i = 0; i < 3; i++) {
         hrData[i] = senSerial.read();
-        delay(10);
       }
     }
   }
+}
+
+
+// Calibrate sensor
+bool calibrateSensor() {
+  senSerial.write(calib, sizeof(calib));
+
+  for (size_t i = 0; i < 50; i++) {
+    while (!senSerial.available());
+
+    while (senSerial.available()) {
+      byte a = senSerial.read();
+      if (int(a) == 254) {
+        byte b;
+        for (size_t i = 0; i < 3; i++) {
+          b = senSerial.read();
+        }
+        if (b == 0x00) {
+          return true;
+        } else if (b == 0x02) {
+          return false;
+        }
+      }
+    }
+  }
+  return false;
 }
